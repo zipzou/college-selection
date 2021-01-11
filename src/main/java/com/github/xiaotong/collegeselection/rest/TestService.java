d317bf4f835989eb15a9cd5fef05d763760f0d50
Zip Zou <zouzhipeng.1@163.com>
2021-01-11 13:00:17 +0800

    convert spring conf file from .properties to .yml

--- src/main/java/com/github/xiaotong/collegeselection/rest/TestService.java
+++ src/main/java/com/github/xiaotong/collegeselection/rest/TestService.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestService {
  
+  /**
+   * 测试请求
+   * @return 请求的JSON字符串
+   */
   @GetMapping(value = "/hello")
   public String hello() {
     Map<String, String> msg = new HashMap<String, String>();
@@ -19,6 +23,10 @@ public class TestService {
     return JSON.toJSONString(msg);
   }
 
+  /**
+   * 测试请求
+   * @return 请求返回的数据Body
+   */
   @GetMapping(value="/hellobody")
   public ResponseData<Object> body() {
     return ResponseData.success("Hello SpringBoot!(with Body)");
